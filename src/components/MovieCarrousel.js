91e105e9a41e5299d4e1bab33f09306e1db281c3
Hugo Pina <hugopina.2000@gmail.com>
2022-04-12 12:41:10 -0300

    Finished Movie Carrousel

--- src/components/MovieCarrousel.js
+++ src/components/MovieCarrousel.js
@@ -1,17 +1,57 @@
 import "../styles/movieCarrousel.css";
 import MovieContent from "./MovieContent.js";
 
-function movieCarrousel() {
+function MovieCarrousel() {
+  let actualPanel = 0;
+
+  let handleRightClick = () => {
+    let positions = ["0vw", "-90vw", "-180vw"];
+    let panels = document.getElementsByClassName("panel");
+    for (let i = 0; i < panels.length; i++) {
+      if (actualPanel < 2) {
+        panels.item(i).style.transform = `translateX(${
+          positions[actualPanel + 1]
+        })`;
+      }
+    }
+    console.log("Direita");
+    actualPanel++;
+  };
+
+  let handleLeftClick = () => {
+    let positions = ["0vw", "-90vw", "-180vw"];
+    let panels = document.getElementsByClassName("panel");
+    for (let i = 0; i < panels.length; i++) {
+      if (actualPanel > 0) {
+        panels.item(i).style.transform = `translateX(${
+          positions[actualPanel - 1]
+        })`;
+      }
+    }
+    console.log("esquerda");
+    actualPanel--;
+  };
+
   return (
     <>
       <h1>Lançamentos</h1>
-      <div className="carroussel">
-        <MovieContent />
-        <MovieContent />
-        <MovieContent />
+      <div className="container">
+        <button className="arrow aleft" onClick={handleLeftClick} />
+        <div className="carroussel">
+          <div className="panel">
+            <MovieContent movieTitle="The Boys" />
+          </div>
+          <div className="panel">
+            <MovieContent movieTitle="The Girls" />
+          </div>
+          <div className="panel">
+            <MovieContent movieTitle="The Kids" />
+          </div>
+        </div>
+        <button className="arrow aright" onClick={handleRightClick} />
       </div>
     </>
   );
 }
 
-export default movieCarrousel;
+export default MovieCarrousel;
--- src/components/MovieContent.js
+++ src/components/MovieContent.js
@@ -1,11 +1,11 @@
 import "../styles/movieContent.css";
 
-const movieContent = () => {
+const movieContent = (props) => {
   return (
     <div className="content">
-      <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+      <iframe src="https://www.youtube.com/embed/q5l-2R7E_48"></iframe>
       <div className="info">
-        <h2>Nome do Filme</h2>
+        <h2>{props.movieTitle}</h2>
         <p className="sinopse">
           <b>Sinopse:</b> Lorem ipsum dolor sit amet, consectetur adipiscing
           elit. Nullam est nulla, vulputate ac mollis in, semper eu massa. Ut ut
